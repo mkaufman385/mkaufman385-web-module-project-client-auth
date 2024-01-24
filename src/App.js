75a0f319ec801ff1e633ea29a3f4cbd8deec6798
mkaufman385 <mkaufman385@gmail.com>
2024-01-24 14:36:05 -0500

    Wednesday App.js push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const Login = () => {
+  return <h2>Login</h2>;
+};
+
+const FriendsList = () => {
+  return <h2>FriendsList</h2>;
+};
+const AddFriend = () => {
+  return <h2>AddFriend</h2>;
+};
 
 function App() {
   return (
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <Routes>
+        <Route path="/">
+          <Login />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/friends">
+          <FriendsList />
+        </Route>
+        <Route path="/friends/add">
+          <AddFriend />
+        </Route>
+      </Routes>
     </div>
   );
 }
